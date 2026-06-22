36468be9950b836ba361d2a0c4a56cd44072e809
abolivarfwdcostarica-blip <abolivarfwdcostarica@gmail.com>
2026-06-22 15:31:40 -0600

    feat(login,register):cambio de estilos

--- FrontEnd/app/[locale]/(public)/layout.tsx
+++ FrontEnd/app/[locale]/(public)/layout.tsx
@@ -3,5 +3,9 @@ export default function PublicLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <div className="bg-secondary min-h-screen">
+      {children}
+    </div>
+  );
 }
